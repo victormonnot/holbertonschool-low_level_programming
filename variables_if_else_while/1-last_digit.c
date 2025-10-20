591fe25bf5e5a70e317a14a659e851cda2e929c9
Victor Monnot <victormonnot@protonmail.com>
2025-10-20 23:11:16 +0200

    fix: Changed a number in a printf output

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,17 +11,17 @@
 int main(void)
 {
 	int n;
-	int lastdigit;
+	int lastdig;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
+	lastdig = n % 10;
 
-	if (lastdigit > 5)
-		printf("Last digit of %d is %d and is greater than 0\n", n, lastdigit);
-	else if (lastdigit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
-	else if (lastdigit < 6 && lastdigit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+	if (lastdig > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
+	else if (lastdig == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastdig);
+	else if (lastdig < 6 && lastdig != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
 	return (0);
 }
