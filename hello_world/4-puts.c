ab4b1298cc30170bedb16f0e16bc6a65df982560
Victor Monnot <victormonnot@protonmail.com>
2025-10-20 15:06:24 +0200

    fix: changing the comments once again

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-/* bah c'est la fonction main c'est ça qu'elle veut betty ? */
+/**
+ * File: 4-puts.c
+ * Auth: Victor
+ * Desc: Programme qui affiche "Programming is like building a multilingual puzzle" avec puts et pas printf
+ */
+/**
+ * main - point d’entrée du programme
+ *
+ * Return: Toujours 0 (succès)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
