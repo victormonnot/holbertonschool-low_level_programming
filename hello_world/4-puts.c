1c5c135dfb38fbb7abcfd5754fc23bd0628514d8
Victor Monnot <victormonnot@protonmail.com>
2025-10-20 14:50:53 +0200

    fix: Changing the comment, please betty..

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main(void) /* bah c'est la fonction main c'est ça qu'elle veut betty ? */
+/* bah c'est la fonction main c'est ça qu'elle veut betty ? */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
