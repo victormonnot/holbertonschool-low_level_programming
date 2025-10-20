f7f576bcda9b4dc95cb807873880e3671f5680bf
Victor Monnot <victormonnot@protonmail.com>
2025-10-20 14:40:53 +0200

    fix: Adding a comment for the fonction to try to please Betty again..

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) //bah c'est la fonction main c'est ça qu'elle veut betty ?
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
