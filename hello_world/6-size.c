6457661b92b6bcc5e99bff222a7e4c3b090c4c67
Victor Monnot <victormonnot@protonmail.com>
2025-10-20 15:46:38 +0200

    feat: Created a c progam the size of differents variables

---
+++ hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Afficher la taille en bytes de plusieurs variables
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int charsize = sizeof(char);
+    int intsize = sizeof(int);
+    int longintsize = sizeof(long int);
+    int longlongintsize = sizeof(long long int);
+    int floatsize = sizeof(float);
+
+    printf("Size of a char: %d byte(s)\n", charsize);
+    printf("Size of an int: %d byte(s)\n", intsize);
+    printf("Size of a long int: %d byte(s)\n", longintsize);
+    printf("Size of a long long int: %d byte(s)\n", longlongintsize);
+    printf("Size of a float: %d byte(s)\n", floatsize);
+	return (0);
+}
