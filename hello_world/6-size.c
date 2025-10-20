31362b2772592930f75f6b6253f31288ef7d6705
Victor Monnot <victormonnot@protonmail.com>
2025-10-20 15:49:06 +0200

    fix: removing the spaces so Betty is happy, I hope..

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-    int charsize = sizeof(char);
-    int intsize = sizeof(int);
-    int longintsize = sizeof(long int);
-    int longlongintsize = sizeof(long long int);
-    int floatsize = sizeof(float);
+	int charsize = sizeof(char);
+	int intsize = sizeof(int);
+	int longintsize = sizeof(long int);
+	int longlongintsize = sizeof(long long int);
+	int floatsize = sizeof(float);
 
-    printf("Size of a char: %d byte(s)\n", charsize);
-    printf("Size of an int: %d byte(s)\n", intsize);
-    printf("Size of a long int: %d byte(s)\n", longintsize);
-    printf("Size of a long long int: %d byte(s)\n", longlongintsize);
-    printf("Size of a float: %d byte(s)\n", floatsize);
+	printf("Size of a char: %d byte(s)\n", charsize);
+	printf("Size of an int: %d byte(s)\n", intsize);
+	printf("Size of a long int: %d byte(s)\n", longintsize);
+	printf("Size of a long long int: %d byte(s)\n", longlongintsize);
+	printf("Size of a float: %d byte(s)\n", floatsize);
 	return (0);
 }
