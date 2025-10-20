62122ebd3a3cc3930910c2e7adad0ef32dd66db2
Victor Monnot <victormonnot@protonmail.com>
2025-10-20 15:09:11 +0200

    fix: changing the commenting style for betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * File: 4-puts.c
  * Auth: Victor
- * Desc: Programme qui affiche "Programming is like building a multilingual puzzle" avec puts et pas printf
+ * Desc: Afficher ""Programming is like..." avec puts et pas printf
  */
 /**
  * main - point d’entrée du programme
