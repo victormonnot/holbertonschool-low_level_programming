fb5d5076045d4a647a0f3d06f1626a63b48c0abe
Victor Monnot <victormonnot@protonmail.com>
2025-10-21 10:38:23 +0200

    feat: Created a c program that print all the numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : Affiche tout les nombres a de base 16
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'F'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
