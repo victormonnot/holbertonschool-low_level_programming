2c14b38a16424d969a3ada1bc5ee6fb9e3af3436
Victor Monnot <victormonnot@protonmail.com>
2025-10-21 10:57:12 +0200

    feat: Created a c program that print all single digit number separated by a comma and a space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : Affiche tout les nombres a un digit de base 10
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
