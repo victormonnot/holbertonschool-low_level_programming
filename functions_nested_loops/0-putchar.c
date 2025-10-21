f38d43e4ec42f594761c0c9e8e9d7cb024001034
Victor Monnot <victormonnot@protonmail.com>
2025-10-21 16:12:44 +0200

    feat: Created a simple c program that print a word

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : Affiche tout les chiffres avec des ", " entre eux
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
