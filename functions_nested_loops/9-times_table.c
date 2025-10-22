f64eb4c8c4998ab9cf43a6b6ca2ac31827098240
Victor Monnot <victormonnot@protonmail.com>
2025-10-22 21:41:28 +0200

    feat: Created a c function that print the 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - Entry point
+ *
+ * Description : Affiche la table de 9
+ * @n: le nombre a check
+ *
+ * Return: Toujours 0 (succès)
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = i * j;
+
+			if (r < 10)
+			{
+				if (j <= 0)
+				{
+					_putchar(r + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(r + '0');
+				}
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+
+			}
+			if (j != 9)
+			_putchar (',');
+
+		}
+		_putchar('\n');
+	}
+}
