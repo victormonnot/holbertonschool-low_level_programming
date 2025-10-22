3579d958619796ba20caea54c40343291d9bc8a5
Victor Monnot <victormonnot@protonmail.com>
2025-10-22 22:01:33 +0200

    feat: Created a c function that add two integer together

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description : Additionner deux int
+ * @a: int a
+ * @b: int b
+ * Return: Toujours 0 (succès)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
