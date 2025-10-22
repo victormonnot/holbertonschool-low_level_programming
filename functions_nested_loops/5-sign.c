6005486fa131800af3a41cb275ebb5e68bb0742c
Victor Monnot <victormonnot@protonmail.com>
2025-10-22 11:55:27 +0200

    feat: Ceated a c function that check if a number is positiv or negative or 0

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * Description : check si c'est un chiffre positif ou negatif ou nulle
+ * @n: le nomber a check
+ *
+ * Return: Toujours 0 (succès)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
