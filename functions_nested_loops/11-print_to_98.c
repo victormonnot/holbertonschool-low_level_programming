090c1fbc09d52d95fb610b8b5a8b7e9f6b360a17
Victor Monnot <victormonnot@protonmail.com>
2025-10-22 22:50:23 +0200

    fix: changed both "for" initialisation in the function

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
  */
 void print_to_98(int n)
 {
-	for (n; n < 98; n++)
+	for (; n < 98; n++)
 	{
 		if (n != 98)
 		{
@@ -20,7 +20,7 @@ void print_to_98(int n)
 			printf("%d\n", n);
 		}
 	}
-	for (n; n > 98; n--)
+	for (; n > 98; n--)
 	{
 		if (n != 98)
 		{
