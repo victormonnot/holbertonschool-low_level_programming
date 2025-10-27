6087720831abd5701a499438b81acdf155927393
Victor Monnot <victormonnot@protonmail.com>
2025-10-27 10:08:38 +0100

    feat: Created a c function that prints the numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Description : multiplie 2 integer
+ *
+ * Return: Toujours 0 (succès)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar (i + '0');
+	}
+	putchar ('\n');
+}
