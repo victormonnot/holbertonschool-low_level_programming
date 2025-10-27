fe6d7f89f68170509a71c821a1284410fc6f69df
Victor Monnot <victormonnot@protonmail.com>
2025-10-27 09:47:44 +0100

    feat: created a c function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * Description : multiplie 2 integer
+ * @a: le premier int
+ * @b: le deuxième int
+ * Return: Toujours 0 (succès)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
