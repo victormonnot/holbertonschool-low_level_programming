fc26d6bae36b37df35e0d2802fd7419f9c30db31
Victor Monnot <victormonnot@protonmail.com>
2025-10-28 10:09:35 +0100

    feat: created a c function that prints a string, followed by a new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - Entry point
+ *
+ * Description : affiche un string puis saute une ligne
+ * @str: charactère a afficher
+ *
+ * Return: Toujours 0 (succès)
+ */
+void _puts(char *str)
+{
+	int longueur = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
