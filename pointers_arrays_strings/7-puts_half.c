6be5212a910d448c0d15532a59bc579bf51d4ef3
Victor Monnot <victormonnot@protonmail.com>
2025-10-28 19:59:50 +0100

    feat: Created a c function that  print the second half of the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - Entry point
+ *
+ * Description : affiche la deuxième moitié d'un string
+ * @str: string
+ *
+ * Return: Toujours 0 (succès)
+ */
+void puts_half(char *str)
+{
+	int longueur;
+	int i;
+	int caca;
+
+	while (str[longueur] != '\0')
+	{
+		longueur++;
+	}
+	if (longueur % 2 == 0)
+	{
+		i = longueur / 2;
+		for (; i < longueur; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (longueur % 2 ==1)
+	{
+		i = (longueur + 1) / 2;
+		for (; i < longueur; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
