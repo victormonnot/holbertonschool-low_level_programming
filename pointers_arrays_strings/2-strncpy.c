c0965cfd3008c46f23d50646acc9b7c49b2ae38c
Victor Monnot <victormonnot@protonmail.com>
2025-10-29 15:26:37 +0100

    feat: Created a c function that copies a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - Entry point
+ *
+ * Description : copie le string pointé de src vers dest
+ * @dest: copie vers ça
+ * @src: copie depuis ça
+ * @n: taille
+ *
+ * Return: Toujours 0 (succès)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		{
+			dest[i] = src[i];
+		}
+	}
+	return (dest);
+}
