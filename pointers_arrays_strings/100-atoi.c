14af5edbbb6c40e412738fa7b6b65d6f387e14a1
Victor Monnot <victormonnot@protonmail.com>
2025-10-31 14:39:18 +0100

    feat: initial commit for this function with nothing in it

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - Entry point
+ *
+ * Description : reverse le contenu d'une array d'int
+ * @s: le nombre d'éléments
+ *
+ * Return: Toujours 0 (succès)
+ */
+int _atoi(char *s)
+{
+	return (0);
+}
