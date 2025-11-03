d8e58c7ee43a189764088fd54e6a91b44adf3e23
Victor Monnot <victormonnot@protonmail.com>
2025-11-03 16:28:29 +0100

    feat: Created a c function that locates a substring.

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strstr - Entry point
+ *
+ * Description : fills memory with a constant byte.
+ * @haystack: a
+ * @needle: a
+ *
+ * Return: Toujours 0 (succès)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *h != '\0')
+		{
+			*h++;
+			*n++;
+		}
+		if (*h == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
