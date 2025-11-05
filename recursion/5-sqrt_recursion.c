238da084b276b6b9a14e43573367170f76d8bc50
Victor Monnot <victormonnot@protonmail.com>
2025-11-05 15:23:13 +0100

    feat: Created a c function that returns the natural square root of a number.

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * vrai_sqrt_recursion - pour trouver
+ * @n: number
+ * @i: pour calculer
+ *
+ * Return: .
+ */
+int vrai_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (vrai_sqrt_recursion(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ *
+ * Return: .
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (vrai_sqrt_recursion(n, 0));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -55,5 +55,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 void print_diagsums(int *a, int size);
+int _sqrt_recursion(int n);
 
 #endif
