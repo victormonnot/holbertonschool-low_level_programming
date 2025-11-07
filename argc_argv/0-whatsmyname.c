47b8d07623d642db164b47b6fd5d65d0c06c589a
Victor Monnot <victormonnot@protonmail.com>
2025-11-07 14:21:51 +0100

    feat: Created a c a program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Affiche le nom du programme
+ * @argc: le nombre d'arguments
+ * @argv: l'array d'arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+#README du directory argc_argv
