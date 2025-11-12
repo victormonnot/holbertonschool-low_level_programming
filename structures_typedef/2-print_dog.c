e6a1e8904345ff75d4e54aba154300d9bb34a0ea
Victor Monnot <victormonnot@protonmail.com>
2025-11-12 19:10:33 +0100

    feat: Created a c function that prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - affiche le contenu d'une structure dog
+ * @d: pointeur vers la structure
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	d->name;
+	d->age;
+	d->owner;
+
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name : (nil)\n");
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner != NULL)
+	{
+		printf("Bob: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
+}
