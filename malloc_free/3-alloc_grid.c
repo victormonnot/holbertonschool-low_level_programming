b1f75d86edb887bb6d18ed302e182fe4d98f12c5
Victor Monnot <victormonnot@protonmail.com>
2025-11-12 10:43:17 +0100

    feat: Created a c function that returns a pointer to a 2 dimensional array of integers

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * alloc_grid - retourne un pointeur
+ * @width: la largeur de la grille
+ * @height: la hauteur de la grille
+ *
+ * Return: .
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -60,6 +60,7 @@ int is_prime_number(int n);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 
 #endif
