9cbffbb821583e5f9a8ad1a6f7f41d52e06b6002
Victor Monnot <victormonnot@protonmail.com>
2025-11-12 18:14:22 +0100

    feat: created a c struct for a dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+#README du directory structues_typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - pour un chien
+ * @name:  nom du chien
+ * @age:   âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Description: Les infos d'un chien
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
