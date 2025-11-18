c40e2a7e6afb55fd7f8389508d9049b7c894ff39
Victor Monnot <victormonnot@protonmail.com>
2025-11-18 11:12:50 +0100

    feat: Created a c function that searches for an integer.

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - .
+ * @array: .
+ * @size: .
+ * @cmp: .
+ *
+ * Return: .
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+}
