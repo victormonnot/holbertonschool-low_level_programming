004cc714aae84b4e05f5edbf1f0401731f6bc500
Victor Monnot <victormonnot@protonmail.com>
2025-11-21 21:53:38 +0100

    feat: Created a c program that performs simple operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integers.
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: resultat de l'operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two integers.
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: resultat de l'operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers.
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: resultat de l'operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two integers.
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: resultat de l'operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulo of two integers.
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: resultat de l'operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
