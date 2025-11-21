632bb1f1936e8dc2436d8eb1627741a7b2ea692a
Victor Monnot <victormonnot@protonmail.com>
2025-11-21 10:52:14 +0100

    feat: Created a c function that returns the sum of all its parameters

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns la somme de tous ses paramètres
+ * @n: nombre d'arguments
+ *
+ * Return: la somme de tous ses paramètres
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
