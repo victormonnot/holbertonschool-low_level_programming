7707a1d3b634543654a3a1b919fb7096d1e10449
Victor Monnot <victormonnot@protonmail.com>
2025-12-03 10:32:41 +0100

    fix: added a library to use strdup

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,10 +1,13 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: double pointer to the head of the list
+ * add_node - adds a new node at the beginning
+ * @head: pointer pointer to the head of the list
  * @str: string to be duplicated and added
- * Return: address of the new element, or NULL if it failed
+ * Return: the new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
